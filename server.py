42a3ca97407101bc1761a28d0f36d0cb38745b00
zunaira51 <bsef14m516@pucit.edu.pk>
2016-05-27 06:11:39 +0500

    Update server.py

--- server.py
+++ server.py
@@ -1,54 +1,58 @@
-'''
-    Simple socket server using threads
-'''
- 
+
 import socket
 import sys
 from thread import *
- 
-HOST = ''   # Symbolic name meaning all available interfaces
-PORT = 8667 # Arbitrary non-privileged port
- 
+
+HOST = ''
+PORT = 8277
+arr=[]
+i=0
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 print 'Socket created'
- 
-#Bind socket to local host and port
+
+
 try:
     s.bind((HOST, PORT))
 except socket.error as msg:
     print 'Bind failed. Error Code : ' + str(msg[0]) + ' Message ' + msg[1]
     sys.exit()
-     
-print 'Socket bind complete'
- 
-#Start listening on socket
-s.listen(2)
-print 'Server  now listening'
- 
-#Function for handling connections. This will be used to create threads
-def clientthread(conn):
-    #Sending message to connected client
-#    conn.send('Welcome to the Server.\n') #send only takes string
 
-    #infinite loop so that function do not terminate and thread do not end.
+print 'Socket bind complete'
+
+
+s.listen(10)
+print 'Socket now listening'
+
+
+# Function for handling connections. This will be used to create threads
+def clientthread(conn):
+    # Sending message to connected client
+    conn.send('Welcome to the server. Type something and hit enter\n')
+
     while True:
-        #Receiving from client
+        # Receiving from client
         data = conn.recv(1024)
-        reply='User Said: '+data
-	print conn,reply,'\n'
-        if not data: 
+        reply =  data
+        if not data:
             break
-        conn.sendall(reply)
-     #	print 'Data send to all clients \n'
-    #came out of loop
-    #conn.close()
- 
-#now keep talking with the client
+
+        if conn == arr[0]:
+            arr[1].sendall(reply)
+        else:
+            arr[0].sendall(reply)
+
+    # came out of loop
+    conn.close()
+
+# now keep talking with the client
 while 1:
-    #wait to accept a connection - blocking call
+    # wait to accept a connection - blocking call
     conn, addr = s.accept()
-    print 'Connected with ' ,addr  
-    #start new thread takes 1st argument as a function name to be run, second is the tuple of arguments to the function.
-    start_new_thread(clientthread ,(conn,))
- 
+    arr.append(conn)
+    print 'Connected with ' + addr[0] + ':' + str(addr[1])
+
+    # start new thread takes 1st argument as a function name to be run, second is the tuple of arguments to the function.
+    start_new_thread(clientthread, (conn,))
+    i+=1
 s.close()
+
